a93b84d6292c0b0d4a6f0957d2b728d45a2ba819
sanatha-ltech <sanathad@lolctech.com>
2022-07-01 12:56:14 +0530

    added random generators for STAN and RET Ref

--- src/main/java/com/example/j8583/RandomNumberGenerator.java
+++ src/main/java/com/example/j8583/RandomNumberGenerator.java
@@ -9,11 +9,15 @@ public class RandomNumberGenerator {
         //public String getUniqueStan() {
             int stanLength = 6;
             String sTime = ThreadLocalRandom.current().nextInt(999999) + "" + new Date().getTime();
+        System.out.println("sTime 01 "+sTime);
             sTime = new StringBuilder(sTime).reverse().toString();
+        System.out.println("sTime 02 "+sTime);
             StringBuilder stringBuilder = new StringBuilder(sTime.substring(0, stanLength));
-
+        System.out.println("stringBuilder "+stringBuilder);
             int randPosition = ThreadLocalRandom.current().nextInt(stanLength);
+        System.out.println("randPosition "+randPosition);
             char randNumber = (char)(ThreadLocalRandom.current().nextInt(10) + 48);
+        System.out.println("randNumber "+randNumber);
             stringBuilder.setCharAt(randPosition, randNumber);
 
             //return stringBuilder.toString();
